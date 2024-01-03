0dee6f5b28f80feb6f318dfa9c432959aec46b4a
domodan <ongomdaniel9@gmail.com>
2024-01-03 15:03:07 +0000

    a function named createReportObject whose parameter, employeesList, is the return value of the previous function createEmployeesObject

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,14 +1,10 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {
-    ...employeesList,
-  };
-
-  const getNumberOfDepartments = () => {
-    return Object.keys(allEmployees).length;
-  };
-
   return {
-    allEmployees,
-    getNumberOfDepartments,
+    allEmployees: {
+      ...employeesList
+    },
+    getNumberOfDepartments: function (employeesList) {
+      return Object.keys(employeesList).length;
+    }
   };
 }
