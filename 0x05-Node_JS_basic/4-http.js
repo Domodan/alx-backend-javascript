45335acdcd93e58f7f7188171b52c8e058c77555
Domodan <ongomdaniel9@gmail.com>
2024-03-01 18:49:46 +0000

    created a small HTTP server using the http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,25 @@
+/**
+ * Create a small HTTP server using the http module
+ * @returns {void}
+ * @author Daniel Ongom <https://github.com/Domodan>
+ */
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
