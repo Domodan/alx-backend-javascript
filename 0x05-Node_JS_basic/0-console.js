c3ae7ae5af5f3970d0431930df5281c88fdb4b08
Domodan <ongomdaniel9@gmail.com>
2024-03-01 17:04:29 +0000

    created a function named displayMessage that prints in STDOUT the string argument

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+const displayMessage = (message) => {
+	console.log(message);
+}
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+module.exports = displayMessage;
