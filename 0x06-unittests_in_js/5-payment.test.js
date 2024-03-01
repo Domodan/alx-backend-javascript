bf379dd7af27117bf163ec812b55fa5fc038e72a
Domodan <ongomdaniel9@gmail.com>
2024-03-01 21:35:58 +0000

    copied the the code from 4-payment.js into a new file 5-payment.js: (same content/same behavior)

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -17,13 +17,13 @@ describe('sendPaymentRequestToApi', () => {
 
   it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(sinonSpy.log.calledWith('The total is: 120')).to.be.true;
+    expect(sinonSpy.log.calledOnce).to.be.true;
   });
 
   it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToApi(10, 10);
-    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
-    expect(bigBrother.log.calledOnce).to.be.true;
+    expect(sinonSpy.log.calledWith('The total is: 20')).to.be.true;
+    expect(sinonSpy.log.calledOnce).to.be.true;
   });
 });
